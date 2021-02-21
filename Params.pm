8aed92c40910b9c1e441b00775fcd04237f32c7f
Michal Josef Špaček <tupinek@gmail.com>
2021-02-21 15:02:06 +0100

    Prepare for 0.07 version

--- Params.pm
+++ Params.pm
@@ -11,7 +11,7 @@ use Scalar::Util qw(blessed);
 # Export.
 Readonly::Array our @EXPORT_OK => qw(params);
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Params processing.
 sub params {
@@ -298,6 +298,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
